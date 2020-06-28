d5c0e242c4a925f4fccf50763f1824abbc46c651
netohelvecio <helvecioneto77@gmail.com>
2020-06-28 12:58:51 -0300

    feat: relationship between users and appointments

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -4,15 +4,23 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './Users';
 
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  provider: string;
+  @Column('uuid')
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
